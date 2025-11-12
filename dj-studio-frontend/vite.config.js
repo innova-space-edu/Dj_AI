be7b7d6759961473198d701ba7e7169ff054d09e
Innova Space <emorales@colprovidencia.cl>
2025-11-12 11:20:13 -0300

    Update vite.config.js

--- dj-studio-frontend/vite.config.js
+++ dj-studio-frontend/vite.config.js
@@ -1,3 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins:[react()], server:{ port:5173 } })
+
+export default defineConfig({
+  base: './',           // importante para Netlify
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+  server: { port: 5173 }
+})
